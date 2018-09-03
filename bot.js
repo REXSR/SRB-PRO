32396a6d25a6a8bbbf87f35509c41bbfd99f23a6
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-03 05:26:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,21 @@
- const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '-'
-
+const prefix = '+'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Great. Games`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
 });
+
+client.on('ready', () => {
+    client.user.setActivity("Great Games",{type: 'streaming'})
+});
+
+
+
+
+
+
+
+
 
 let points = JSON.parse(fs.readFileSync('points.json', 'utf8'));
 
